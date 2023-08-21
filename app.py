c0f1813b00c1d7540d63bb575221bf3903901734
Rodrigo Siqueira <34777347+rasiqueira@users.noreply.github.com>
2023-08-20 21:02:14 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,145 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+from langchain.prompts.prompt import PromptTemplate
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI( model="gpt-4-0314", openai_api_key="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")
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    custom_template = """Você é um redator especializado em posts de redes sociais a maioria ds posts são sobre temas médicos você vai receber documentos de treinamento com informações na maioria das vezes acadêmicas. Siga as instruções para gerar posts que engagem a audiência porém se cheque sempre as informações de acordo com seus documentos de treinamento.
+                    Chat History:
+                    {chat_history}
+                    Follow Up Input: {question}
+                    Standalone question:"""
+    CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        condense_question_prompt=CUSTOM_QUESTION_PROMPT,
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] == st.secrets["passwords"]['username'] and st.session_state["password"] == st.secrets["passwords"]["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+
+
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    load_dotenv()
+    st.set_page_config(page_title="Crie com seus PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Crie com seus PDFs :books:")
+    user_question = st.text_input("""Vanessa, digite o tipo de post, a rede social e o público que deseja atingir:""")
+    if user_question:
+        try:
+            handle_userinput(user_question)
+        except:
+            st.error('Não se esqueça de clicar em Processar', icon="🚨")
+
+    with st.sidebar:
+        st.subheader("Seus documentos")
+        pdf_docs = st.file_uploader(
+            "Faça o upoload dos seus documentos e clique em 'Processar' e aguardar a finalização do processo de treinamento.", accept_multiple_files=True)
+        if st.button("Processar"):
+            with st.spinner("Processando"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+
